789f271739d59e9eb8ddb6fe9fd5ca24c65d3c92
SurionA <frere.maxime@gmail.com>
2017-04-01 22:15:25 +0200

    #UPDATE# refactor

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const request = require('request');
-const jsdom = require("jsdom");
-const Q = require("q");
+const jsdom = require('jsdom');
+const Q = require('q');
 const dotenv = require('dotenv-safe');
-const url = require('url');
+const path = require('path');
+
+const startDate = '2015-09';
+const endDate = '2017-04';
 
 const edfParticulierLoginUrl = 'https://particulier.edf.fr/bin/edf_rc/servlets/authentication';
 const edfEquilibreStatusUrl = 'https://particulier.edf.fr/bin/edf_rc/servlet/context/equilibreStatus';
 const edfSamlUrl = 'https://equilibre.edf.fr/saml-sp';
-const edfEquilibreUrl = 'https://equilibre.edf.fr/api/v1/sites/-/monthly-elec-consumptions?begin-month=2015-09&end-month=2017-04&ended=false';
+const edfEquilibreUrl = `https://equilibre.edf.fr/api/v1/sites/-/monthly-elec-consumptions?begin-month=${startDate}&end-month=${endDate}&ended=false`;
 
 const CONSTANTS = {
   edfLoginRequestName: 'EDF_LOGIN',
@@ -15,22 +18,23 @@ const CONSTANTS = {
   edfSsoRequestName: 'EDF_SSO',
   edfSamlRequestName: 'EDF_SAML',
   edfEquilibreRequestName: 'EDF_EQUILIBRE',
-}
+  edfEquilibreBaseHash: '#/login?',
+};
 
 const edfLoginOptions = {
   uri: edfParticulierLoginUrl,
   method: 'POST',
-  form: {}
+  form: {},
 };
 
 const edfEquilibreStatusOptions = {
   uri: edfEquilibreStatusUrl,
   method: 'POST',
-  headers: {}
+  headers: {},
 };
 
 const edfSsoOptions = {
-  headers: {}
+  headers: {},
 };
 
 const edfSAMLOptions = {
@@ -38,20 +42,25 @@ const edfSAMLOptions = {
   method: 'POST',
   followAllRedirects: true,
   headers: {},
-  form: {}
+  form: {},
+};
+
+const edfEquilibreOptions = {
+  uri: edfEquilibreUrl,
+  headers: {},
 };
 
 let requestCookies;
 
 dotenv.load({
-  path: __dirname + '/config/.env',
-  sample: __dirname + '/config/.env.example',
-  allowEmptyValues: false
+  path: path.join(__dirname, 'config/.env'),
+  sample: path.join(__dirname, 'config/.env.example'),
+  allowEmptyValues: false,
 });
 
 edfLoginOptions.form = {
   login: process.env.EDF_LOGIN,
-  password: process.env.EDF_PASSWORD
+  password: process.env.EDF_PASSWORD,
 };
 
 Q.nfcall(request, edfLoginOptions)
@@ -61,12 +70,12 @@ Q.nfcall(request, edfLoginOptions)
   .then(setSAMLResponse)
   .then(edfSamlCall)
   .then(edfEquilibreCall)
-  .then(response => {
+  .then((response) => {
     handlerError(response[0], CONSTANTS.edfEquilibreRequestName);
 
-    console.log('MY CONSOMMATION response.headers',response[0].headers);
-    console.log('MY CONSOMMATION response.statusCode',response[0].statusCode);
-    console.log('MY CONSOMMATION',response[0].body);
+    console.log('MY CONSOMMATION response.headers', response[0].headers);
+    console.log('MY CONSOMMATION response.statusCode', response[0].statusCode);
+    console.log('MY CONSOMMATION', response[0].body);
   })
   .catch(err => console.log(err));
 
@@ -84,7 +93,9 @@ function edfSsoCall(response) {
 
   const edfEquilibreStatusResponse = response[0].body;
 
-  if (!edfEquilibreStatusResponse.isActiveEquilibre || !edfEquilibreStatusResponse.isEquilibreServiceSubscribed || !edfEquilibreStatusResponse.isEligibleToEquilibreService) {
+  if (!edfEquilibreStatusResponse.isActiveEquilibre ||
+      !edfEquilibreStatusResponse.isEquilibreServiceSubscribed ||
+      !edfEquilibreStatusResponse.isEligibleToEquilibreService) {
     throw new Error('Equilibre Service is not available. Please active it on your edf admin panel');
   }
 
@@ -97,17 +108,17 @@ function edfSsoCall(response) {
 function edfSAMLCookie(response) {
   handlerError(response[0], CONSTANTS.edfSsoRequestName);
 
-  requestCookies = requestCookies + ';'  + response[0].headers['set-cookie'].join(';');
+  requestCookies = `${requestCookies};${response[0].headers['set-cookie'].join(';')}`;
   edfSAMLOptions.headers.cookie = requestCookies;
 
   return response[0].body;
 }
 
 function setSAMLResponse(body) {
-  return Q.nfcall(jsdom.env, body, ["http://code.jquery.com/jquery.js"]);
+  return Q.nfcall(jsdom.env, body, ['http://code.jquery.com/jquery.js']);
 }
 
-function edfSamlCall (window) {
+function edfSamlCall(window) {
   edfSAMLOptions.form.SAMLResponse = window.$("input[name='SAMLResponse']").val();
 
   return Q.nfcall(request, edfSAMLOptions);
@@ -117,37 +128,37 @@ function edfEquilibreCall(response) {
   handlerError(response[0], CONSTANTS.edfSamlRequestName);
   const queries = parseUrl(response[0].request.uri.hash);
 
-  return Q.nfcall(request, {url: edfEquilibreUrl, headers: {cookie: requestCookies, Authorization: `Bearer ${queries.t}`}});
+  edfEquilibreOptions.headers.cookie = requestCookies;
+  edfEquilibreOptions.headers.Authorization = `Bearer ${queries.t}`;
+
+  return Q.nfcall(request, edfEquilibreOptions);
 }
 
 function parseUrl(hash) {
-  let HashKeyValueParsed = {};
+  const HashKeyValueParsed = {};
 
-  hash.substring(8).split('&').forEach(function (x) {
-    let arr = x.split('=');
-    arr[1] && (HashKeyValueParsed[arr[0]] = arr[1]);
+  hash.substring(CONSTANTS.edfEquilibreBaseHash.length).split('&').forEach((x) => {
+    const arr = x.split('=');
+    return arr[1] && (HashKeyValueParsed[arr[0]] = arr[1]);
   });
 
   return HashKeyValueParsed;
 }
 
 function handlerError(response, requestName) {
-  if(response.statusCode && response.statusCode !== 200 && response.statusCode !== 302) {
-    console.log('body', response.body);
-    throw `Error while ${requestName}. Server send HTTP ${response.statusCode}`;
+  if (response.statusCode && response.statusCode !== 200 && response.statusCode !== 302) {
+    throw new Error(`Error while ${requestName}. Server send HTTP ${response.statusCode}`);
   }
 
-  if(typeof response.body  !== 'object') {
+  if (typeof response.body !== 'object') {
     try {
       response.body = JSON.parse(response.body);
-    } catch(err) {
+    } catch (err) {
       return;
     }
   }
 
-  if(response.body && response.body.errorCode && response.body.Status !== 200) {
-    throw `Error while ${requestName}. Server send HTTP ${response.body.Status} with: ${response.body.errorLabel}`;
+  if (response.body && response.body.errorCode && response.body.Status !== 200) {
+    throw new Error(`Error while ${requestName}. Server send HTTP ${response.body.Status} with: ${response.body.errorLabel}`);
   }
-
-  return;
 }
